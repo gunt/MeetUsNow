be85c320d712f6b448f2585e3d05b48313402bbb
gunt <4261629+gunt@users.noreply.github.com>
2020-02-03 14:11:23 +0100

    EventList_repeat_test

---
+++ src/EventList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class EventList extends Component {
+  render() {
+    return (
+        <ul className="EventList">
+        </ul>
+    );
+  }
+}
+
+export default EventList;
