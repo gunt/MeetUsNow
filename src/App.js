f84b9bf421c9d7adcea48779ed6a6584478bace9
gunt <4261629+gunt@users.noreply.github.com>
2020-02-03 16:06:28 +0100

    Scenario_2 CitySearch Test

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import EventList from './EventList';
+import CitySearch from './CitySearch';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <CitySearch />
         <EventList />
       </div>
     );
   }
 }
-
 export default App;
