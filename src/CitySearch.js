f707d1d0853c881151a45590749986ccc3020f47
gunt <4261629+gunt@users.noreply.github.com>
2020-02-03 17:29:45 +0100

    Scenario_2 CitySearchTest pass 100%

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -3,20 +3,27 @@ import React, { Component } from 'react';
 class CitySearch extends Component {
 
     state = {
-        query: '',
+        query: 'Munich',
         suggestions: [],
+      }
+
+      handleInputChanged = (event) => {
+        const value = event.target.value;
+        this.setState({ query: value });
+      }
+    
+      handleItemClicked = (value) => {
+        this.setState({ query: value });
       }
 
   render() {
     return (
         <div className="CitySearch">
-  <input
-  type="text"
-  className="city"
-  value={this.state.query}
-/>
-  <ul className="suggestions">
-  </ul>
+        <input type="text" className="city" onChange={this.handleInputChanged} value={this.state.query} />
+        <ul className="suggestions">
+          {this.state.suggestions.map(item => 
+            <li onClick={() => this.handleItemClicked(item.name_string)}  key={item.name_string}>{item.name_string}</li>)}
+        </ul>
 </div>
     );
   }
--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -17,4 +17,52 @@ describe('<CitySearch /> component', () => {
       const query = CitySearchWrapper.state('query');
       expect(CitySearchWrapper.find('.city').prop('value')).toBe(query);
     });
-  });
+    test('change state when text input changes', () => {
+        const eventObject = { target: { value: 'Berlin' } };
+        CitySearchWrapper.find('.city').simulate('change', eventObject);
+        expect(CitySearchWrapper.state('query')).toBe('Berlin');
+      });
+      test('render list of suggestions correctly', () => {
+        const suggestions = CitySearchWrapper.state('suggestions');
+        expect(CitySearchWrapper.find('.suggestions li')).toHaveLength(
+          suggestions.length
+        );
+        for (let i = 0; i < suggestions.length; i += 1) {
+          expect(
+            CitySearchWrapper.find('.suggestions li')
+              .at(i)
+              .text()
+          ).toBe(suggestions[i].name_string);
+        }
+      });
+      test('click on suggestion should change query state', () => {
+        CitySearchWrapper.setState({
+          suggestions: [
+            {
+              city: 'Munich',
+              country: 'de',
+              localized_country_name: 'Germany',
+              name_string: 'Munich, Germany',
+              zip: 'meetup3',
+              lat: 48.14,
+              lon: 11.58
+            },
+            {
+              city: 'Munich',
+              country: 'us',
+              localized_country_name: 'USA',
+              state: 'ND',
+              name_string: 'Munich, North Dakota, USA',
+              zip: '58352',
+              lat: 48.66,
+              lon: -98.85
+            }
+          ]
+        });
+    
+        CitySearchWrapper.find('.suggestions li')
+          .at(0)
+          .simulate('click');
+        expect(CitySearchWrapper.state('query')).toBe('Munich, Germany');
+      });
+    });
