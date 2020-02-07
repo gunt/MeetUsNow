5435a63549be3abfce786d300c1eb19ad15f8c14
gunt <4261629+gunt@users.noreply.github.com>
2020-02-07 19:19:28 +0100

    4.5 Acceptance filterEventsByCity.test.js Done

--- src/App.js
+++ src/App.js
@@ -8,19 +8,37 @@ import { getEvents } from './api';
 class App extends Component {
   state = {
     events: [],
+    lat: null,
+    lon: null,
+    page: null,
   }
-  // To get a list of events based on the user’s location, 
-  // don’t specify a location in the request. If “lat” and “lon” aren’t specified, 
-  // Meetup will return a list of events based on the location from which the request was made (i.e., the user’s location).
-  updateEvents = (lat, lon) => {
-    getEvents(lat, lon).then(events => this.setState({ events }));
+
+  componentDidMount() {
+    this.updateEvents();
+  }
+
+  updateEvents = (lat, lon, page) => {
+    // We use state to store value of lat, lon, page if user has changed it.
+    if (lat && lon) {
+      getEvents(lat, lon, this.state.page).then(events =>
+        this.setState({ events, lat, lon })
+      );
+    } else if (page) {
+      getEvents(this.state.lat, this.state.lon, page).then(events =>
+        this.setState({ events, page })
+      );
+    } else {
+      getEvents(this.state.lat, this.state.lon, this.state.page).then(events =>
+        this.setState({ events })
+      );
+    }
   }
 
   render() {
     return (
       <div className="App">
         <CitySearch updateEvents={this.updateEvents} />
-        <NumberOfEvents />
+        <NumberOfEvents updateEvents={this.updateEvents} />
         <EventList events={this.state.events} />
       </div>
     );
