b1bc58b81b288bf2b1745d0ded5c0acb8a97673b
gunt <4261629+gunt@users.noreply.github.com>
2020-02-09 15:59:06 +0100

    4.6 Atatus detect error

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as atatus from 'atatus-spa';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
@@ -10,3 +11,4 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+atatus.config('ee3642ca61994c90ba5a13a07da30fa4').install();
