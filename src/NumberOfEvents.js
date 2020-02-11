8d13636ed88c22f306104d4ac44b2a6767d2e36d
gunt <4261629+gunt@users.noreply.github.com>
2020-02-11 11:49:01 +0100

    4.7 Info_Alert & Error_Alert 100% works

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,28 +1,40 @@
 import React, { Component } from 'react';
+import { ErrorAlert } from './Alert';
 
 class NumberOfEvents extends Component {
   state = {
     query: 32,
   };
 
-  handleInputChanged = (event) => {
+  //according to the 4.7 exercise here the code need to change instead 
+  //of the handleInputChanged like the cityseach component for the info alert
+  // this time for the Error Alert
+  onNumberChanged = (event) => {
     const value = event.target.value;
-    this.setState({ query: value });
-    // this.props.updateEvents(null, null, value);
+    this.setState({ number: value });
+    this.props.updateEvents(null, null, value);
+    if (value < 1) {
+      this.setState({ errorText: 'Number should be at least 1' });
+    } else {
+      this.setState({ errorText: '' });
+    }
   }
 
   render() {
     return (
-      <div className="numberOfEvents">
+      <div className="NumberOfEvents">
+        <ErrorAlert text={this.state.errorText} />
+        {/* That was tricky how to make the text show - focus here */}
+        <span>Show </span>
         <input
           type="number"
-          className="numberEvents"
-          onChange={this.handleInputChanged}
-          value={this.state.query}
+          className="number-of-events"
+          onChange={this.onNumberChanged}
+          value={this.state.number}
         />
-        <span> Events </span>
+        <span> Events</span>
       </div>
-    )
+    );
   }
 }
 
